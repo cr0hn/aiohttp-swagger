0264285b7ff06636a0637f16cd3389566f57add8
Tuomas Virtanen <katajakasa@gmail.com>
2019-10-20 15:33:19 +0300

    Fix file handle leak when building from file

--- aiohttp_swagger/helpers/builders.py
+++ aiohttp_swagger/helpers/builders.py
@@ -155,7 +155,8 @@ def generate_doc_from_each_end_point(
 
 
 def load_doc_from_yaml_file(doc_path: str):
-    loaded_yaml = yaml.load(open(doc_path, "r", encoding="utf-8").read(), Loader=yaml.FullLoader)
+    with open(doc_path, "r", encoding="utf-8") as f:
+        loaded_yaml = yaml.load(f.read(), Loader=yaml.FullLoader)
     return json.dumps(loaded_yaml)
 
 
